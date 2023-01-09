519a440c380ec7424523f667dea5249d5472a674
salta33 <zhangaliysal@gmail.com>
2023-01-09 18:27:54 +0600

    completed 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output="";
+    if (num >=0){
+    for (i=1; i <=num; i++){
+output+=string;
+    }
+return output;}
+else {
+    return "ERROR";
+}
 };
 
 // Do not edit below this line
