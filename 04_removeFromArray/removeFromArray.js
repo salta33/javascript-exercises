ef47edf0cae2d4a8a103cf3d71f2f071c8d01d68
salta33 <zhangaliysal@gmail.com>
2023-01-10 15:27:57 +0600

    removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const array=args[0];
+    return array.filter(val => !args.includes(val));
 
-};
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
