5ea9fb3d2e39346bce5732dfb4bbc1486dcabd56
salta33 <zhangaliysal@gmail.com>
2023-01-10 16:18:38 +0600

    sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum=0;
 
-};
+    if(num2>num1){
+    for (let j=num2;j>=num1;j--){
+     sum+=j;
+    }}
+ else if (num1>num2){
+for (let j=num1; j>=num2;j--){
+    sum+=j;
+} } 
+
+if(!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+if(num1 <0 || num2 <0) return "ERROR";
+
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
+    
