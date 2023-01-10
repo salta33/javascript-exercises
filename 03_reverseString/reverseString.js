9178fbe6ea1ace00fec15650666cf8eb4391155d
salta33 <zhangaliysal@gmail.com>
2023-01-10 12:18:19 +0600

    solved reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split("");
+let reverseStr = "";
+    for(let j=array.length-1; j >=0; j--){
+reverseStr+=array[j];
+    }
+    return reverseStr;
 };
 
 // Do not edit below this line
